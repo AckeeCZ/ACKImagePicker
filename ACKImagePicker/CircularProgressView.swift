a0503e733866e8b08bbe064da31701f56ca37854
Lukáš Hromadník <lukas.hromadnik@ackee.cz>
2020-08-28 17:26:39 +0200

    ✨ Add CircularProgressView

--- ACKImagePicker/CircularProgressView.swift
+++ ACKImagePicker/CircularProgressView.swift
@@ -1,92 +1,64 @@
-//
-//  RoundProgressView.swift
-//  ACKImagePicker
-//
-//  Created by Lukáš Hromadník on 28/08/2020.
-//  Copyright © 2020 Lukáš Hromadník. All rights reserved.
-//
-
 import UIKit
 
 /// Progress view that is in the shape of a circle
-public class CircularProgressView: UIView {
+///
+/// It draws itself within specified rect.
+/// The size is derived from the size of the rect where the smaller dimension (width / height) is used.
+/// The view itslef is drawn in the middle of the rect.
+/// As a primary color is used `tintColor` of the view.
+public final class CircularProgressView: UIView {
     
     /// Current progress
     ///
     /// This value should be in the range between 0 and 1.
     /// If the value is out of the range its automatically adjusted.
-    var progress: CGFloat = 0 {
+    public var progress: CGFloat = 0 {
         didSet {
             // Update the progress if the value is out of the range
             progress = max(0, min(1, progress))
             
-            // Perform animation from the old to the new state
-            animateFrom(oldValue, to: progress)
+            // Update the progress
+            progressLayer.isHidden = false
+            progressLayer.strokeEnd = progress
         }
     }
     
     /// Line width of the circular progress view
-    var lineWidth: CGFloat = 2
+    public var lineWidth: CGFloat = 3
     
-    // Layer on which the progress animation is performed
+    /// Color applied on the background circle
+    public var secondaryTintColor: UIColor = .lightGray
+    
+    /// Layer that shows the current progress
     private var progressLayer: CAShapeLayer!
     
     // MARK: - Drawing
     
     public override func draw(_ rect: CGRect) {
         let circularPath = UIBezierPath(
-            arcCenter: CGPoint(x: rect.size.width / 2.0, y: rect.size.height / 2.0),
+            arcCenter: CGPoint(x: rect.size.width / 2, y: rect.size.height / 2),
             radius: min(rect.size.width, rect.size.height),
             startAngle: -.pi / 2,
             endAngle: 3 * .pi / 2,
             clockwise: true
         )
         
-        let backgroundLayer = circleLayer(path: circularPath, strokeColor: .lightGray)
+        let backgroundLayer = CAShapeLayer()
+        backgroundLayer.path = circularPath.cgPath
+        backgroundLayer.fillColor = UIColor.clear.cgColor
+        backgroundLayer.lineCap = .round
+        backgroundLayer.lineWidth = lineWidth
+        backgroundLayer.strokeColor = secondaryTintColor.cgColor
         layer.addSublayer(backgroundLayer)
         
-        let progressLayer = circleLayer(path: circularPath, strokeColor: .red)
+        let progressLayer = CAShapeLayer()
+        progressLayer.path = circularPath.cgPath
+        progressLayer.fillColor = UIColor.clear.cgColor
+        progressLayer.lineCap = .round
+        progressLayer.lineWidth = lineWidth
+        progressLayer.strokeColor = tintColor.cgColor
         progressLayer.isHidden = true
         layer.addSublayer(progressLayer)
         self.progressLayer = progressLayer
-    }
-    
-    // MARK: - Helpers
-    
-    /// Factory for creating circle layers
-    ///
-    /// Both background and foreground layers have some same properties
-    ///
-    /// - Parameters
-    ///   - `path`: Circular path that specifies the final shape
-    ///   - `strokeColor`: Color of the circle
-    ///   - `lineWidth`: Width of the circle
-    /// - Returns: Final circular shape layer with applied parameters
-    private func circleLayer(path: UIBezierPath, strokeColor: UIColor) -> CAShapeLayer {
-        let circleLayer = CAShapeLayer()
-        circleLayer.path = path.cgPath
-        circleLayer.fillColor = UIColor.clear.cgColor
-        circleLayer.lineCap = .round
-        circleLayer.lineWidth = lineWidth
-        circleLayer.strokeColor = strokeColor.cgColor
-        
-        return circleLayer
-    }
-    
-    /// Animates the progress from the old value to the new value
-    ///
-    /// - Parameters
-    ///   - `oldProgress`: The old value
-    ///   - `newProgress`: The new value
-    private func animateFrom(_ oldProgress: CGFloat, to newProgress: CGFloat) {
-        progressLayer.isHidden = false
-        
-        let circularProgressAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        circularProgressAnimation.duration = 0.3
-        circularProgressAnimation.fromValue = oldProgress
-        circularProgressAnimation.toValue = newProgress
-        circularProgressAnimation.fillMode = .forwards
-        circularProgressAnimation.isRemovedOnCompletion = false
-        progressLayer.add(circularProgressAnimation, forKey: "progressAnim")
     }
 }
--- ACKImagePicker/ViewControllers/BaseViewController.swift
+++ ACKImagePicker/ViewControllers/BaseViewController.swift
@@ -9,51 +9,83 @@
 import UIKit
 
 class BaseViewController: UIViewController {
-    
     private weak var loadingView: UIView!
     private weak var activityIndicator: UIActivityIndicatorView!
+    private weak var progressView: CircularProgressView!
     
     override func loadView() {
         super.loadView()
         
-        let loadingView = UIView()
-        if #available(iOS 13.0, *) {
-            loadingView.backgroundColor = .systemBackground
-        } else {
-            loadingView.backgroundColor = .white
-        }
-        loadingView.layer.cornerRadius = 10
-        view.addSubview(loadingView)
-        loadingView.makeCenterEqualToSuperview()
-        self.loadingView = loadingView
+//        let loadingView = UIView()
+//        if #available(iOS 13.0, *) {
+//            loadingView.backgroundColor = .systemBackground
+//        } else {
+//            loadingView.backgroundColor = .white
+//        }
+//        loadingView.layer.cornerRadius = 10
+//        view.addSubview(loadingView)
+//        loadingView.makeCenterEqualToSuperview()
+//        self.loadingView = loadingView
+//
+//        let activityIndicator = UIActivityIndicatorView(style: .whiteLarge)
+//        if #available(iOS 13.0, *) {
+//            activityIndicator.color = .label
+//        } else {
+//            activityIndicator.color = .black
+//        }
+//        loadingView.addSubview(activityIndicator)
+//        activityIndicator.makeEdgesEqualToSuperview(insets: UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20))
+//        self.activityIndicator = activityIndicator
+//
+//        stopLoadingAnimation()
         
-        let activityIndicator = UIActivityIndicatorView(style: .whiteLarge)
-        if #available(iOS 13.0, *) {
-            activityIndicator.color = .label
-        } else {
-            activityIndicator.color = .black
-        }
-        loadingView.addSubview(activityIndicator)
-        activityIndicator.makeEdgesEqualToSuperview(insets: UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20))
-        self.activityIndicator = activityIndicator
-        
-        stopLoadingAnimation()
+        let progressView = CircularProgressView()
+//        progressView.isProgressAnimationEnabled = false
+//        progressView.backgroundColor = .red
+//        progressView.mainColor = .blue
+        view.addSubview(progressView)
+//        progressView.makeCenterEqualToSuperview()
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            progressView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            progressView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            progressView.widthAnchor.constraint(equalToConstant: 50),
+            progressView.heightAnchor.constraint(equalToConstant: 50),
+        ])
+        self.progressView = progressView
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        view.bringSubviewToFront(loadingView)
+//        view.bringSubviewToFront(loadingView)
+        view.bringSubviewToFront(progressView)
     }
     
-    internal func startLoadingAnimation() {
-        loadingView.isHidden = false
-        view.bringSubviewToFront(loadingView)
-        activityIndicator.startAnimating()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        progressView.progress = 0.3
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { [weak self] in
+            self?.progressView.progress = 0.75
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.progressView.progress = 0.4
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+            self?.progressView.progress = 1
+        }
     }
     
-    internal func stopLoadingAnimation() {
-        loadingView.isHidden = true
-        activityIndicator.stopAnimating()
+    func startLoadingAnimation() {
+//        loadingView.isHidden = false
+        view.bringSubviewToFront(progressView)
+//        activityIndicator.startAnimating()
+    }
+    
+    func stopLoadingAnimation() {
+//        loadingView.isHidden = true
+//        activityIndicator.stopAnimating()
     }
 }
