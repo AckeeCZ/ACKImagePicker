bf7747b775d77c9379b1c37bedd6943f17d1fb9b
Lukáš Hromadník <lukas.hromadnik@ackee.cz>
2023-04-24 14:56:02 +0200

    🤯 Somebody tries to kill us with constants

--- ACKImagePicker/SSCheckMark.swift
+++ ACKImagePicker/SSCheckMark.swift
@@ -67,7 +67,14 @@ open class SSCheckMark: UIView {
         let frame = self.bounds
         let group = CGRect(x: frame.minX + 3, y: frame.minY + 3, width: frame.width - 6, height: frame.height - 6)
 
-        let ovalPath = UIBezierPath(ovalIn: CGRect(x: group.minX + floor(group.width * 0.00000 + 0.5), y: group.minY + floor(group.height * 0.00000 + 0.5), width: floor(group.width * 1.00000 + 0.5) - floor(group.width * 0.00000 + 0.5), height: floor(group.height * 1.00000 + 0.5) - floor(group.height * 0.00000 + 0.5)))
+        let ovalPath = UIBezierPath(
+            ovalIn: CGRect(
+                x: group.minX,
+                y: group.minY,
+                width: floor(group.width + 0.5),
+                height: floor(group.height + 0.5)
+            )
+        )
 
         context.saveGState()
         context.setShadow(offset: shadow2Offset, blur: CGFloat(shadow2BlurRadius), color: shadow2.cgColor)
@@ -109,7 +116,14 @@ open class SSCheckMark: UIView {
         let shadow2BlurRadius = 2.5
         let frame = self.bounds
         let group = CGRect(x: frame.minX + 3, y: frame.minY + 3, width: frame.width - 6, height: frame.height - 6)
-        let emptyOvalPath = UIBezierPath(ovalIn: CGRect(x: group.minX + floor(group.width * 0.00000 + 0.5), y: group.minY + floor(group.height * 0.00000 + 0.5), width: floor(group.width * 1.00000 + 0.5) - floor(group.width * 0.00000 + 0.5), height: floor(group.height * 1.00000 + 0.5) - floor(group.height * 0.00000 + 0.5)))
+        let emptyOvalPath = UIBezierPath(
+            ovalIn: CGRect(
+                x: group.minX,
+                y: group.minY,
+                width: floor(group.width + 0.5),
+                height: floor(group.height + 0.5)
+            )
+        )
 
         context.saveGState()
         context.setShadow(offset: shadow2Offset, blur: CGFloat(shadow2BlurRadius), color: shadow2.cgColor)
